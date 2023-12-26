a0408a045ff0a0ba8c21400835e81fa1fa9ed256
Tilak <tilakshankar8@gmail.com>
2023-12-26 20:19:47 +0530

    Changed content

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
 
   return (
     <div>
-      <h1 className={`${raleway.className} text-2xl`}>heloooooo</h1>
+      <h1 className={`${raleway.className} text-2xl`}>hehe</h1>
     </div>
 
   )
